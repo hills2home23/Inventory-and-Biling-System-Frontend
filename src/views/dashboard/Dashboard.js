fbe4d2e8cd83da914e287ddef15b8c582ecb186b
Rohit Bhati <rohitbhati8833@gmail.com>
2021-06-11 15:17:34 +0530

    minor changes

--- src/views/dashboard/Dashboard.js
+++ src/views/dashboard/Dashboard.js
@@ -1,13 +1,310 @@
-import React from 'react'
-import { useDispatch, useSelector } from "react-redux";
+import React, { Component } from 'react';
+import { Doughnut } from 'react-chartjs-2';
+import { TodoListComponent } from '../apps/TodoList'
 
-function Dashboard() {
 
-  return (
-    <div>
-      
-    </div>
-  )
+
+export class Dashboard extends Component {
+
+  transactionHistoryData =  {
+    labels: ["Paypal", "Stripe","Cash"],
+    datasets: [{
+        data: [55, 25, 20],
+        backgroundColor: [
+          "#d00550","#00d25b","#ffab00"
+        ]
+      }
+    ]
+  };
+
+  transactionHistoryOptions = {
+    responsive: true,
+    maintainAspectRatio: true,
+    segmentShowStroke: false,
+    cutoutPercentage: 70,
+    elements: {
+      arc: {
+          borderWidth: 0
+      }
+    },      
+    legend: {
+      display: false
+    },
+    tooltips: {
+      enabled: true
+    }
+  }
+
+  sliderSettings = {
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  }
+  toggleProBanner() {
+    document.querySelector('.proBanner').classList.toggle("hide");
+  }
+  render () {
+    return (
+      <div>
+        <div className="row">
+          <div className="col-xl-3 col-sm-6 grid-margin stretch-card">
+            <div className="card">
+              <div className="card-body">
+                <div className="row">
+                  <div className="col-9">
+                    <div className="d-flex align-items-center align-self-start">
+                      <h3 className="mb-0">$12.34</h3>
+                      <p className="text-success ml-2 mb-0 font-weight-medium">+3.5%</p>
+                    </div>
+                  </div>
+                  <div className="col-3">
+                    <div className="icon icon-box-success ">
+                      <span className="mdi mdi-arrow-top-right icon-item"></span>
+                    </div>
+                  </div>
+                </div>
+                <h6 className="text-muted font-weight-normal">Potential growth</h6>
+              </div>
+            </div>
+          </div>
+          <div className="col-xl-3 col-sm-6 grid-margin stretch-card">
+            <div className="card">
+              <div className="card-body">
+                <div className="row">
+                  <div className="col-9">
+                    <div className="d-flex align-items-center align-self-start">
+                      <h3 className="mb-0">$17.34</h3>
+                      <p className="text-success ml-2 mb-0 font-weight-medium">+11%</p>
+                    </div>
+                  </div>
+                  <div className="col-3">
+                    <div className="icon icon-box-success">
+                      <span className="mdi mdi-arrow-top-right icon-item"></span>
+                    </div>
+                  </div>
+                </div>
+                <h6 className="text-muted font-weight-normal">Revenue current</h6>
+              </div>
+            </div>
+          </div>
+          <div className="col-xl-3 col-sm-6 grid-margin stretch-card">
+            <div className="card">
+              <div className="card-body">
+                <div className="row">
+                  <div className="col-9">
+                    <div className="d-flex align-items-center align-self-start">
+                      <h3 className="mb-0">$12.34</h3>
+                      <p className="text-danger ml-2 mb-0 font-weight-medium">-2.4%</p>
+                    </div>
+                  </div>
+                  <div className="col-3">
+                    <div className="icon icon-box-danger">
+                      <span className="mdi mdi-arrow-bottom-left icon-item"></span>
+                    </div>
+                  </div>
+                </div>
+                <h6 className="text-muted font-weight-normal">Daily Income</h6>
+              </div>
+            </div>
+          </div>
+          <div className="col-xl-3 col-sm-6 grid-margin stretch-card">
+            <div className="card">
+              <div className="card-body">
+                <div className="row">
+                  <div className="col-9">
+                    <div className="d-flex align-items-center align-self-start">
+                      <h3 className="mb-0">$31.53</h3>
+                      <p className="text-success ml-2 mb-0 font-weight-medium">+3.5%</p>
+                    </div>
+                  </div>
+                  <div className="col-3">
+                    <div className="icon icon-box-success ">
+                      <span className="mdi mdi-arrow-top-right icon-item"></span>
+                    </div>
+                  </div>
+                </div>
+                <h6 className="text-muted font-weight-normal">Expense current</h6>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-4 grid-margin stretch-card">
+            <div className="card">
+              <div className="card-body">
+                <h4 className="card-title">Transaction History</h4>
+                <div className="aligner-wrapper">
+                  <Doughnut data={this.transactionHistoryData} options={this.transactionHistoryOptions} />
+                  <div className="absolute center-content">
+                    <h5 className="font-weight-normal text-whiite text-center mb-2 text-black">1200</h5>
+                    <p className="text-small text-muted text-center mb-0">Total</p>
+                  </div>
+                </div>  
+                {/* bg-gray-dark -> bg-light */}
+                <div className="bg-light d-flex d-md-block d-xl-flex flex-row py-3 px-4 px-md-3 px-xl-4 rounded mt-3">
+                  <div className="text-md-center text-xl-left">
+                    <h6 className="mb-1">Transfer to Paypal</h6>
+                    <p className="text-muted mb-0">07 Jan 2019, 09:12AM</p>
+                  </div>
+                  <div className="align-self-center flex-grow text-right text-md-center text-xl-right py-md-2 py-xl-0">
+                    <h6 className="font-weight-bold mb-0">$236</h6>
+                  </div>
+                </div>
+                {/* bg-gray-dark -> bg-light */}
+                <div className="bg-light d-flex d-md-block d-xl-flex flex-row py-3 px-4 px-md-3 px-xl-4 rounded mt-3">
+                  <div className="text-md-center text-xl-left">
+                    <h6 className="mb-1">Tranfer to Stripe</h6>
+                    <p className="text-muted mb-0">07 Jan 2019, 09:12AM</p>
+                  </div>
+                  <div className="align-self-center flex-grow text-right text-md-center text-xl-right py-md-2 py-xl-0">
+                    <h6 className="font-weight-bold mb-0">$593</h6>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-4 grid-margin stretch-card">
+            <div className="card">
+              <div className="card-body">
+                <h4 className="card-title">Transaction History</h4>
+                <div className="aligner-wrapper">
+                  <Doughnut data={this.transactionHistoryData} options={this.transactionHistoryOptions} />
+                  <div className="absolute center-content">
+                    <h5 className="font-weight-normal text-whiite text-center mb-2 text-black">1200</h5>
+                    <p className="text-small text-muted text-center mb-0">Total</p>
+                  </div>
+                </div>  
+                {/* bg-gray-dark -> bg-light */}
+                <div className="bg-light d-flex d-md-block d-xl-flex flex-row py-3 px-4 px-md-3 px-xl-4 rounded mt-3">
+                  <div className="text-md-center text-xl-left">
+                    <h6 className="mb-1">Transfer to Paypal</h6>
+                    <p className="text-muted mb-0">07 Jan 2019, 09:12AM</p>
+                  </div>
+                  <div className="align-self-center flex-grow text-right text-md-center text-xl-right py-md-2 py-xl-0">
+                    <h6 className="font-weight-bold mb-0">$236</h6>
+                  </div>
+                </div>
+                {/* bg-gray-dark -> bg-light */}
+                <div className="bg-light d-flex d-md-block d-xl-flex flex-row py-3 px-4 px-md-3 px-xl-4 rounded mt-3">
+                  <div className="text-md-center text-xl-left">
+                    <h6 className="mb-1">Tranfer to Stripe</h6>
+                    <p className="text-muted mb-0">07 Jan 2019, 09:12AM</p>
+                  </div>
+                  <div className="align-self-center flex-grow text-right text-md-center text-xl-right py-md-2 py-xl-0">
+                    <h6 className="font-weight-bold mb-0">$593</h6>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-4 grid-margin stretch-card">
+            <div className="card">
+              <div className="card-body">
+                <h4 className="card-title">Transaction History</h4>
+                <div className="aligner-wrapper">
+                  <Doughnut data={this.transactionHistoryData} options={this.transactionHistoryOptions} />
+                  <div className="absolute center-content">
+                    <h5 className="font-weight-normal text-whiite text-center mb-2 text-black">1200</h5>
+                    <p className="text-small text-muted text-center mb-0">Total</p>
+                  </div>
+                </div>  
+                {/* bg-gray-dark -> bg-light */}
+                <div className="bg-light d-flex d-md-block d-xl-flex flex-row py-3 px-4 px-md-3 px-xl-4 rounded mt-3">
+                  <div className="text-md-center text-xl-left">
+                    <h6 className="mb-1">Transfer to Paypal</h6>
+                    <p className="text-muted mb-0">07 Jan 2019, 09:12AM</p>
+                  </div>
+                  <div className="align-self-center flex-grow text-right text-md-center text-xl-right py-md-2 py-xl-0">
+                    <h6 className="font-weight-bold mb-0">$236</h6>
+                  </div>
+                </div>
+                {/* bg-gray-dark -> bg-light */}
+                <div className="bg-light d-flex d-md-block d-xl-flex flex-row py-3 px-4 px-md-3 px-xl-4 rounded mt-3">
+                  <div className="text-md-center text-xl-left">
+                    <h6 className="mb-1">Tranfer to Stripe</h6>
+                    <p className="text-muted mb-0">07 Jan 2019, 09:12AM</p>
+                  </div>
+                  <div className="align-self-center flex-grow text-right text-md-center text-xl-right py-md-2 py-xl-0">
+                    <h6 className="font-weight-bold mb-0">$593</h6>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-sm-4 grid-margin">
+            <div className="card">
+              <div className="card-body">
+                <h5>Revenue</h5>
+                <div className="row">
+                  <div className="col-8 col-sm-12 col-xl-8 my-auto">
+                    <div className="d-flex d-sm-block d-md-flex align-items-center">
+                      <h2 className="mb-0">$32123</h2>
+                      <p className="text-success ml-2 mb-0 font-weight-medium">+3.5%</p>
+                    </div>
+                    <h6 className="text-muted font-weight-normal">11.38% Since last month</h6>
+                  </div>
+                  <div className="col-4 col-sm-12 col-xl-4 text-center text-xl-right">
+                    <i className="icon-lg mdi mdi-codepen text-primary ml-auto"></i>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-sm-4 grid-margin">
+            <div className="card">
+              <div className="card-body">
+                <h5>Sales</h5>
+                <div className="row">
+                  <div className="col-8 col-sm-12 col-xl-8 my-auto">
+                    <div className="d-flex d-sm-block d-md-flex align-items-center">
+                      <h2 className="mb-0">$45850</h2>
+                      <p className="text-success ml-2 mb-0 font-weight-medium">+8.3%</p>
+                    </div>
+                    <h6 className="text-muted font-weight-normal"> 9.61% Since last month</h6>
+                  </div>
+                  <div className="col-4 col-sm-12 col-xl-4 text-center text-xl-right">
+                    <i className="icon-lg mdi mdi-wallet-travel text-danger ml-auto"></i>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-sm-4 grid-margin">
+            <div className="card">
+              <div className="card-body">
+                <h5>Purchase</h5>
+                <div className="row">
+                  <div className="col-8 col-sm-12 col-xl-8 my-auto">
+                    <div className="d-flex d-sm-block d-md-flex align-items-center">
+                      <h2 className="mb-0">$2039</h2>
+                      <p className="text-danger ml-2 mb-0 font-weight-medium">-2.1% </p>
+                    </div>
+                    <h6 className="text-muted font-weight-normal">2.27% Since last month</h6>
+                  </div>
+                  <div className="col-4 col-sm-12 col-xl-4 text-center text-xl-right">
+                    <i className="icon-lg mdi mdi-monitor text-success ml-auto"></i>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-12 col-xl-4 grid-margin stretch-card">
+            <div className="card">
+              <div className="card-body">
+                <h4 className="card-title">To do list</h4>
+                <TodoListComponent />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div> 
+    );
+  }
 }
 
 export default Dashboard
